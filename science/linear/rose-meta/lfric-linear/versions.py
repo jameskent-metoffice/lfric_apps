94f77d80482571d0d279ac1ab9bfc16171f0e13e
Christine Johnson <74597224+cjohnson-pi@users.noreply.github.com>
2026-01-14 09:23:16 +0000

    correct versions.py upgrade macro

--- science/linear/rose-meta/lfric-linear/versions.py
+++ science/linear/rose-meta/lfric-linear/versions.py
@@ -20,32 +20,35 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
 """
 
+
 class vn30_t108(MacroUpgrade):
-    # Upgrade macro for #108 by Christine Johnson
+    """Upgrade macro for ticket #108 by Christine Johnson."""
 
     BEFORE_TAG = "vn3.0"
     AFTER_TAG = "vn3.0_t108"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-linear
         # Commands from rose-meta/lfric-linear
         scaling = self.get_setting_value(
             config, ["namelist:planet", "scaling_factor"]
         )
         if "125.0" in scaling:
-            self.add_setting(config, ["namelist:linear", "mol_efficiency"], ".false.")
+            self.add_setting(
+                config, ["namelist:linear", "transport_efficiency"], ".false."
+            )
         else:
-            self.add_setting(config, ["namelist:linear", "mol_efficiency"], ".true.")
+            self.add_setting(
+                config, ["namelist:linear", "transport_efficiency"], ".true."
+            )
 
         return config, self.reports
